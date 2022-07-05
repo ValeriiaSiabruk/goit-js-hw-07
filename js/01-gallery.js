f2db90b2a6e2cad83aae46caa36a6eeb9a13372a
Valeriia Siabruk <vsyabryk@gmail.com>
2022-07-05 12:46:34 +0300

    add task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+let modal;
+
+const galleryHTML = galleryItems.map((elem) => {
+  return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${elem.original}">
+        <img
+          class="gallery__image"
+          src="${elem.preview}"
+          data-source="${elem.original}"
+          alt="${elem.description}"
+        >
+      </a>
+    </div>
+  `;
+});
+
+gallery.insertAdjacentHTML("afterbegin", galleryHTML.join(""));
+
+function modalShoweHandler(event) {
+  event.preventDefault();
+
+  const imgSrc = event.target.dataset.source;
+
+  if (!imgSrc) return;
+
+  modal = basicLightbox.create(
+    `<img src="${imgSrc}" width="1280" height="853">`,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", modalCloseHandler);
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", modalCloseHandler);
+      },
+    }
+  );
+
+  modal.show();
+}
+
+function modalCloseHandler(event) {
+  if (event.key === "Escape") {
+    modal.close();
+  }
+}
+
+gallery.addEventListener("click", modalShoweHandler);
